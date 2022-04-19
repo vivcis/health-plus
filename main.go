ddd5e9096488d17ea7552bde41ec434141ed4412
Kjasuquo <joseph.asuquo@decagon.dev>
2022-04-19 20:19:15 +0100

    mux router used for pics and logo

--- main.go
+++ main.go
@@ -3,9 +3,16 @@ package main
 import (
 	"github.com/decadev/squad10/healthplus/db"
 	"github.com/decadev/squad10/healthplus/router"
+	"github.com/joho/godotenv"
+	"log"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	db.SetupDB()
 
 	router.SetupRouter()
